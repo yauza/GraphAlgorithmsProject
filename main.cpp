6255d95d0e0ef10029313f6f3e2c55e02a61c1fd
yauza <61622378+yauza@users.noreply.github.com>
2020-12-01 01:21:20 +0100

    implemented bfs, used in max flow

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <unordered_set>
 #include <unordered_map>
 #include <set>
 #include <vector>
 #include "Graph.h"
+#include <queue>
 
 using namespace std;
 
+bool bfs(Graph* G, int s, int t, vector<int>& parent){
+    queue<int> Q;
+    for(int i = 0; i < G->V; i++) parent[i] = -1;
+    vector<bool> visited(G->V, false);
+
+    visited[s] = true;
+    Q.push(s);
+
+    while(!Q.empty()){
+        int u = Q.back();
+        for(int v : G->edges[u]){
+            if(!visited[v] && G->weights[make_pair(u, v)] > 0){
+                visited[v] = True;
+                parent[v] = u;
+                Q.push(v);
+            }
+        }
+    }
+
+    if(parent[t] != -1) return true;
+    else return false;
+}
+
+int maxflow(Graph* G, int s, int t){
+    int max_flow = 0;
+    vector<int> parent(G->V);
+
+}
 
 bool solve(int n, vector<int> playersScore){
     int King = 0;
